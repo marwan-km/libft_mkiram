bfd265db0af034ece40597152b743ca01f5f61c5
marwan kiram <kirammarwan@gmail.com>
2025-10-16 19:43:37 +0100

    from my laptop

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,33 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkiram <mkiram@student.42.fr>              +#+  +:+       +#+        */
+/*   By: itsmemk <itsmemk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 09:58:42 by mkiram            #+#    #+#             */
-/*   Updated: 2025/10/16 10:24:26 by mkiram           ###   ########.fr       */
+/*   Updated: 2025/10/16 19:42:46 by itsmemk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    size_t i;
-    unsigned char *src1;
-    unsigned char *src2;
+	size_t			i;
+	unsigned char	*src1;
+	unsigned char	*src2;
 
-    src1 = (unsigned char *)s1;
-    src2 = (unsigned char *)s2;
-    if (n == 0)
-        return (0);
-    i = 0;
-    while (i < n)
-    {
-        if (src1[i] != src2[i])
-            return (src1[i] - src2[i]);
-        i++;
-    }
-    return (0);
+	src1 = (unsigned char *)s1;
+	src2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		if (src1[i] != src2[i])
+			return (src1[i] - src2[i]);
+		i++;
+	}
+	return (0);
 }
 // int main()
 // {
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkiram <mkiram@student.42.fr>              +#+  +:+       +#+        */
+/*   By: itsmemk <itsmemk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 10:05:17 by mkiram            #+#    #+#             */
-/*   Updated: 2025/10/15 15:20:08 by mkiram           ###   ########.fr       */
+/*   Updated: 2025/10/16 19:42:35 by itsmemk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,34 @@
 #include <stdio.h>
 #include <string.h>
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t              i;
-    unsigned char       *dst;
-    const unsigned char *sr;
+	size_t i;
+	unsigned char *dst;
+	const unsigned char *sr;
 
-    dst = (unsigned char *)dest;
-    sr = (const unsigned char *)src;
-    if (!dst || !src)
-        return (dest);
-    if (dst == sr)
-        return dest;
-    if (dst >= sr)
-    {
-        while (n > 0)
-        {
-            n--;
-            dst[n] = sr[n];
-        }
-    }
-    else
-    {
-        i = 0;
-        while (i < n)
-        {
-            dst[i] = sr[i];
-            i++;
-        }
-    }
-    return (dest);
+	dst = (unsigned char *)dest;
+	sr = (const unsigned char *)src;
+	if (!dst || !src)
+		return (dest);
+	if (dst == sr)
+		return (dest);
+	if (dst >= sr)
+	{
+		while (n > 0)
+		{
+			n--;
+			dst[n] = sr[n];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			dst[i] = sr[i];
+			i++;
+		}
+	}
+	return (dest);
 }
